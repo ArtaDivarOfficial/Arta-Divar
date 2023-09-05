d0e4d6fa67453b1cc0220022ea8d09d72d978e59
Arta Afghan ITS <Arta.Divar.Official@gmail.com>
2023-09-05 16:28:19 +0430

    bag model finished - rayyan

--- lib/repositories/items_respositories/bags_respository/models/bag_model.dart
+++ lib/repositories/items_respositories/bags_respository/models/bag_model.dart
@@ -1,30 +1,71 @@
 import 'dart:convert';
 
 import 'package:divar/repositories/items_respositories/bags_respository/constants/constants.dart';
-import 'package:divar/repositories/items_respositories/item_repository/constants/constants.dart';
 import 'package:flutter/foundation.dart';
 
 class BagModel {
-  final int itemId;
-  final String itemCustomerId;
-  final int itemCategoryId;
-  final int itemSubCategoryId;
-  final List<String> itemImages;
-  final String itemTitle;
-  final String itemAddress;
-  final String itemTotalPrice;
-  final String itemPriceType;
-  final String itemType;
-  final String itemMaterial;
-  final String itemColor;
-  final String itemDescription;
-  final String itemStatus;
-  final String itemPublishStatus;
-  final String itemSoldStatus;
-  final String itemCreatedAt;
-  final String itemUpdatedAt;
+  final int? itemId;
+  final String? itemCustomerId;
+  final int? itemCategoryId;
+  final int? itemSubCategoryId;
+  final List<String>? itemImages;
+  final String? itemTitle;
+  final String? itemAddress;
+  final String? itemTotalPrice;
+  final int? itemPriceType;
+  final int? itemType;
+  final String? itemMaterial;
+  final String? itemColor;
+  final String? itemDescription;
+  final String? itemStatus;
+  final String? itemPublishStatus;
+  final String? itemSoldStatus;
+  final String? itemCreatedAt;
+  final String? itemUpdatedAt;
 
   BagModel({
+    this.itemId,
+    this.itemCustomerId,
+    this.itemCategoryId,
+    this.itemSubCategoryId,
+    this.itemImages,
+    this.itemTitle,
+    this.itemAddress,
+    this.itemTotalPrice,
+    this.itemPriceType,
+    this.itemType,
+    this.itemMaterial,
+    this.itemColor,
+    this.itemDescription,
+    this.itemStatus,
+    this.itemPublishStatus,
+    this.itemSoldStatus,
+    this.itemCreatedAt,
+    this.itemUpdatedAt,
+  });
+
+  BagModel.itemModel({
+    required this.itemId,
+    required this.itemCustomerId,
+    required this.itemCategoryId,
+    required this.itemSubCategoryId,
+    required this.itemImages,
+    required this.itemTitle,
+    required this.itemAddress,
+    required this.itemTotalPrice,
+    required this.itemPriceType,
+    this.itemType = -1,
+    this.itemMaterial = '',
+    this.itemColor = '',
+    this.itemDescription = '',
+    this.itemStatus = '',
+    required this.itemPublishStatus,
+    required this.itemSoldStatus,
+    required this.itemCreatedAt,
+    required this.itemUpdatedAt,
+  });
+
+  BagModel.backpackModel({
     required this.itemId,
     required this.itemCustomerId,
     required this.itemCategoryId,
@@ -45,7 +86,7 @@ class BagModel {
     required this.itemUpdatedAt,
   });
 
-  BagModel.itemModel({
+  BagModel.travelBagModel({
     required this.itemId,
     required this.itemCustomerId,
     required this.itemCategoryId,
@@ -55,11 +96,74 @@ class BagModel {
     required this.itemAddress,
     required this.itemTotalPrice,
     required this.itemPriceType,
-    this.itemType = '',
-    this.itemMaterial = '',
-    this.itemColor = '',
-    this.itemDescription = '',
-    this.itemStatus = '',
+    this.itemType = -1,
+    required this.itemMaterial,
+    required this.itemColor,
+    required this.itemDescription,
+    required this.itemStatus,
+    required this.itemPublishStatus,
+    required this.itemSoldStatus,
+    required this.itemCreatedAt,
+    required this.itemUpdatedAt,
+  });
+
+  BagModel.purseModel({
+    required this.itemId,
+    required this.itemCustomerId,
+    required this.itemCategoryId,
+    required this.itemSubCategoryId,
+    required this.itemImages,
+    required this.itemTitle,
+    required this.itemAddress,
+    required this.itemTotalPrice,
+    required this.itemPriceType,
+    this.itemType = -1,
+    required this.itemMaterial,
+    required this.itemColor,
+    required this.itemDescription,
+    required this.itemStatus,
+    required this.itemPublishStatus,
+    required this.itemSoldStatus,
+    required this.itemCreatedAt,
+    required this.itemUpdatedAt,
+  });
+
+  BagModel.walletModel({
+    required this.itemId,
+    required this.itemCustomerId,
+    required this.itemCategoryId,
+    required this.itemSubCategoryId,
+    required this.itemImages,
+    required this.itemTitle,
+    required this.itemAddress,
+    required this.itemTotalPrice,
+    required this.itemPriceType,
+    this.itemType = -1,
+    required this.itemMaterial,
+    required this.itemColor,
+    required this.itemDescription,
+    required this.itemStatus,
+    required this.itemPublishStatus,
+    required this.itemSoldStatus,
+    required this.itemCreatedAt,
+    required this.itemUpdatedAt,
+  });
+
+  BagModel.otherBagModel({
+    required this.itemId,
+    required this.itemCustomerId,
+    required this.itemCategoryId,
+    required this.itemSubCategoryId,
+    required this.itemImages,
+    required this.itemTitle,
+    required this.itemAddress,
+    required this.itemTotalPrice,
+    required this.itemPriceType,
+    this.itemType = -1,
+    required this.itemMaterial,
+    required this.itemColor,
+    required this.itemDescription,
+    required this.itemStatus,
     required this.itemPublishStatus,
     required this.itemSoldStatus,
     required this.itemCreatedAt,
@@ -75,8 +179,8 @@ class BagModel {
     String? itemTitle,
     String? itemAddress,
     String? itemTotalPrice,
-    String? itemPriceType,
-    String? itemType,
+    int? itemPriceType,
+    int? itemType,
     String? itemMaterial,
     String? itemColor,
     String? itemDescription,
@@ -133,62 +237,125 @@ class BagModel {
 
   Map<String, dynamic> toMapItemModel() {
     return <String, dynamic>{
-      ItemConstants.itemId: itemId,
-      ItemConstants.itemCustomerId: itemCustomerId,
-      ItemConstants.itemCategoryId: itemCategoryId,
-      ItemConstants.itemSubCategoryId: itemSubCategoryId,
-      ItemConstants.itemImages: itemImages,
-      ItemConstants.itemTitle: itemTitle,
-      ItemConstants.itemAddress: itemAddress,
-      ItemConstants.itemTotalPrice: itemTotalPrice,
-      ItemConstants.itemPriceType: itemPriceType,
-      ItemConstants.itemPublishStatus: itemPublishStatus,
-      ItemConstants.itemSoldStatus: itemSoldStatus,
-      ItemConstants.itemCreatedAt: itemCreatedAt,
-      ItemConstants.itemUpdatedAt: itemUpdatedAt,
+      BagConstants.itemId: itemId,
+      BagConstants.itemCustomerId: itemCustomerId,
+      BagConstants.itemCategoryId: itemCategoryId,
+      BagConstants.itemSubCategoryId: itemSubCategoryId,
+      BagConstants.itemImages: itemImages,
+      BagConstants.itemTitle: itemTitle,
+      BagConstants.itemAddress: itemAddress,
+      BagConstants.itemTotalPrice: itemTotalPrice,
+      BagConstants.itemPriceType: itemPriceType,
+      BagConstants.itemPublishStatus: itemPublishStatus,
+      BagConstants.itemSoldStatus: itemSoldStatus,
+      BagConstants.itemCreatedAt: itemCreatedAt,
+      BagConstants.itemUpdatedAt: itemUpdatedAt,
     };
   }
 
   factory BagModel.fromMap(Map<String, dynamic> map) {
     return BagModel(
-      itemId: map[BagConstants.itemId] as int,
-      itemCustomerId: map[BagConstants.itemCustomerId] as String,
-      itemCategoryId: map[BagConstants.itemCategoryId] as int,
-      itemSubCategoryId: map[BagConstants.itemSubCategoryId] as int,
-      itemImages:
-          List<String>.from((map[BagConstants.itemImages] as List<String>)),
-      itemTitle: map[BagConstants.itemTitle] as String,
-      itemAddress: map[BagConstants.itemAddress] as String,
-      itemTotalPrice: map[BagConstants.itemTotalPrice] as String,
-      itemPriceType: map[BagConstants.itemPriceType] as String,
-      itemType: map[BagConstants.itemType] as String,
-      itemMaterial: map[BagConstants.itemMaterial] as String,
-      itemColor: map[BagConstants.itemColor] as String,
-      itemDescription: map[BagConstants.itemDescription] as String,
-      itemStatus: map[BagConstants.itemStatus] as String,
-      itemPublishStatus: map[BagConstants.itemPublishStatus] as String,
-      itemSoldStatus: map[BagConstants.itemSoldStatus] as String,
-      itemCreatedAt: map[BagConstants.itemCreatedAt] as String,
-      itemUpdatedAt: map[BagConstants.itemUpdatedAt] as String,
+      itemId: map[BagConstants.itemId] != null
+          ? map[BagConstants.itemId] as int
+          : null,
+      itemCustomerId: map[BagConstants.itemCustomerId] != null
+          ? map[BagConstants.itemCustomerId] as String
+          : null,
+      itemCategoryId: map[BagConstants.itemCategoryId] != null
+          ? map[BagConstants.itemCategoryId] as int
+          : null,
+      itemSubCategoryId: map[BagConstants.itemSubCategoryId] != null
+          ? map[BagConstants.itemCategoryId] as int
+          : null,
+      itemImages: map[BagConstants.itemImages] != null
+          ? List<String>.from((map[BagConstants.itemImages] as List<String>))
+          : null,
+      itemTitle: map[BagConstants.itemTitle] != null
+          ? map[BagConstants.itemTitle] as String
+          : null,
+      itemAddress: map[BagConstants.itemAddress] != null
+          ? map[BagConstants.itemAddress] as String
+          : null,
+      itemTotalPrice: map[BagConstants.itemTotalPrice] != null
+          ? map[BagConstants.itemTotalPrice] as String
+          : null,
+      itemPriceType: map[BagConstants.itemPriceType] != null
+          ? map[BagConstants.itemPriceType] as int
+          : null,
+      itemType: map[BagConstants.itemType] != null
+          ? map[BagConstants.itemType] as int
+          : null,
+      itemMaterial: map[BagConstants.itemMaterial] != null
+          ? map[BagConstants.itemMaterial] as String
+          : null,
+      itemColor: map[BagConstants.itemColor] != null
+          ? map[BagConstants.itemColor] as String
+          : null,
+      itemDescription: map[BagConstants.itemDescription] != null
+          ? map[BagConstants.itemDescription] as String
+          : null,
+      itemStatus: map[BagConstants.itemStatus] != null
+          ? map[BagConstants.itemStatus] as String
+          : null,
+      itemPublishStatus: map[BagConstants.itemPublishStatus] != null
+          ? map[BagConstants.itemPublishStatus] as String
+          : null,
+      itemSoldStatus: map[BagConstants.itemSoldStatus] != null
+          ? map[BagConstants.itemSoldStatus] as String
+          : null,
+      itemCreatedAt: map[BagConstants.itemCreatedAt] != null
+          ? map[BagConstants.itemCreatedAt] as String
+          : null,
+      itemUpdatedAt: map[BagConstants.itemUpdatedAt] != null
+          ? map[BagConstants.itemUpdatedAt] as String
+          : null,
     );
   }
 
   factory BagModel.fromMapItemModel(Map<String, dynamic> map) {
     return BagModel.itemModel(
-      itemId: map[ItemConstants.itemId] as int,
-      itemCustomerId: map[ItemConstants.itemCustomerId] as String,
-      itemCategoryId: map[ItemConstants.itemCategoryId] as int,
-      itemSubCategoryId: map[ItemConstants.itemSubCategoryId] as int,
-      itemImages:
-          List<String>.from((map[ItemConstants.itemImages] as List<String>)),
-      itemTitle: map[ItemConstants.itemTitle] as String,
-      itemAddress: map[ItemConstants.itemAddress] as String,
-      itemTotalPrice: map[ItemConstants.itemTotalPrice] as String,
-      itemPriceType: map[ItemConstants.itemPriceType] as String,
-      itemPublishStatus: map[ItemConstants.itemPublishStatus] as String,
-      itemSoldStatus: map[ItemConstants.itemSoldStatus] as String,
-      itemCreatedAt: map[ItemConstants.itemCreatedAt] as String,
-      itemUpdatedAt: map[ItemConstants.itemUpdatedAt] as String,
+      itemId: map[BagConstants.itemId] != null
+          ? map[BagConstants.itemId] as int
+          : null,
+      itemCustomerId: map[BagConstants.itemCustomerId] != null
+          ? map[BagConstants.itemCustomerId] as String
+          : null,
+      itemCategoryId: map[BagConstants.itemCategoryId] != null
+          ? map[BagConstants.itemCategoryId] as int
+          : null,
+      itemSubCategoryId: map[BagConstants.itemSubCategoryId] != null
+          ? map[BagConstants.itemCategoryId] as int
+          : null,
+      itemImages: map[BagConstants.itemImages] != null
+          ? List<String>.from((map[BagConstants.itemImages] as List<String>))
+          : null,
+      itemTitle: map[BagConstants.itemTitle] != null
+          ? map[BagConstants.itemTitle] as String
+          : null,
+      itemAddress: map[BagConstants.itemAddress] != null
+          ? map[BagConstants.itemAddress] as String
+          : null,
+      itemTotalPrice: map[BagConstants.itemTotalPrice] != null
+          ? map[BagConstants.itemTotalPrice] as String
+          : null,
+      itemPriceType: map[BagConstants.itemPriceType] != null
+          ? map[BagConstants.itemPriceType] as int
+          : null,
+      itemDescription: map[BagConstants.itemDescription] != null
+          ? map[BagConstants.itemDescription] as String
+          : null,
+      itemPublishStatus: map[BagConstants.itemPublishStatus] != null
+          ? map[BagConstants.itemPublishStatus] as String
+          : null,
+      itemSoldStatus: map[BagConstants.itemSoldStatus] != null
+          ? map[BagConstants.itemSoldStatus] as String
+          : null,
+      itemCreatedAt: map[BagConstants.itemCreatedAt] != null
+          ? map[BagConstants.itemCreatedAt] as String
+          : null,
+      itemUpdatedAt: map[BagConstants.itemUpdatedAt] != null
+          ? map[BagConstants.itemUpdatedAt] as String
+          : null,
     );
   }
 
