4e1dc639f5f86085648e8f7194ba7f0cf3a9f188
Arta Afghan ITS <Arta.Divar.Official@gmail.com>
2023-09-10 11:52:55 +0430

    my advertisements repository finished - rayyan

---
+++ lib/repositories/items_respositories/my_advertisements_repository/constants/constants.dart
@@ -0,0 +1,27 @@
+class MyAdvertisementsConstants {
+  // API Constants
+  static const String apiAddMyAdvertisements = '/add_my_advertisements';
+  static const String apiShowMyAdvertisementsDetails =
+      '/show_my_advertisements_details';
+  static const String apiEditMyAdvertisements = '/edit_my_advertisements';
+  static const String apiDeleteMyAdvertisements = '/delete_my_advertisements';
+  static const String apiShowAllMyAdvertisements =
+      '/show_all_my_advertisements';
+  static const String apiSearchMyAdvertisements = '/search_my_advertisements';
+  static const String apiMyAdvertisementsImages = '/my_advertisements_images';
+  static const String apiSoldMyAdvertisements = '/sold_my_advertisements';
+  // Model Constants
+  static const String itemId = 'item_id';
+  static const String itemCustomerId = 'item_customer_id';
+  static const String itemCategoryId = 'item_cateogry_id';
+  static const String itemSubCategoryId = 'item_sub_category_id';
+  static const String itemImages = 'item_images';
+  static const String itemTitle = 'item_title';
+  static const String itemAddress = 'item_address';
+  static const String itemTotalPrice = 'item_total_price';
+  static const String itemPriceType = 'item_price_type';
+  static const String itemPublishStatus = 'item_publish_status';
+  static const String itemSoldStatus = 'item_status_sold';
+  static const String itemCreatedAt = 'item_created_at';
+  static const String itemUpdatedAt = 'item_updated_at';
+}
